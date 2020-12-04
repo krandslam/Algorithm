992ef97b1b4a57e67f66264819942341fa1cecd4
krandslam <58777020+krandslam@users.noreply.github.com>
2020-12-04 22:02:05 +0900

    Update 2609.cpp

--- BOJ/2609.cpp
+++ BOJ/2609.cpp
@@ -2,34 +2,28 @@
 
 using namespace std;
 
+int GCD(int a, int b);
+
 int main() {
     // 출력 속도 상승
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int A, B,a,b;
+    int A, B;
     cin >> A >> B;
-    a = A;
-    b = B;
 
     // 최대공약수 구함
-    for (int i = 1; i < min(A,B); i++) {
-        if (B == 0) break;
-
-        int r = A%B;
-        A = B;
-        B = r;
-    }
-
-    int G;
-    if (B == 0) G = A;
-        // 서로소
-    else G = 1;
+    int G = GCD(A,B);
 
     // 최소공배수 공식
-    int L = a*b/G;
+    int L = A*B/G;
 
     cout << G << '\n' << L << '\n';
 
     return 0;
+}
+
+int GCD(int a, int b) {
+    if (b == 0) return a;
+    else return GCD(b,a%b);
 }
