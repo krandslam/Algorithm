8e5180bb449fe8f7300762d1abdca77221c8ec1d
krandslam <58777020+krandslam@users.noreply.github.com>
2022-12-15 20:13:00 +0900

    Add files via upload

---
+++ Coursera_Princeton/week1/HelloGoodbye.java
@@ -0,0 +1,7 @@
+public class Main
+{
+	public static void main(String[] arg) {
+		System.out.println("Hello " + arg[0] + " and " + arg[1]);
+        System.out.println("Goodbye " + arg[1] + " and " + arg[0]);
+	}
+}
