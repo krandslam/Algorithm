2016a01997a8dfd7036f14cc2bdda94588aa11c0
krandslam <58777020+krandslam@users.noreply.github.com>
2022-12-15 23:56:15 +0900

    Delete Coursera_Princeton directory

--- Coursera_Princeton/week1/HelloGoodbye.java
+++
@@ -1,7 +0,0 @@
-public class Main
-{
-	public static void main(String[] arg) {
-		System.out.println("Hello " + arg[0] + " and " + arg[1]);
-        System.out.println("Goodbye " + arg[1] + " and " + arg[0]);
-	}
-}
